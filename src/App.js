0828910c7c6e8ef3f8316ce2dd6eaf4725c5f28a
sbmart <sbmart73@gmail.com>
2021-05-08 14:44:13 +0300

    intermediate push wo QF of a header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import './App.css';
+// import './App.css';
+import 'antd/dist/antd.css';
 import React, { useState } from 'react'
 import QRCode from 'qrcode.react'
+import { Input, Layout, Typography } from 'antd'
+// const { Title } = Typography
+import { QrcodeOutlined } from '@ant-design/icons'
 
-const salt = 'методы цифровой трансформации в действии!'
+const { Header, Footer, Content } = Layout;
+const salt = '-*методы цифровой трансформации в действии!/-'
+
 function App() {
   const [qrString, setQrString] = useState('')
 
@@ -10,12 +16,30 @@ function App() {
     setQrString(event.target.value)
   }
   return (
-    <div className="App">
-      <input value={qrString} onChange={handleChange} />
-      <h2>Кодируем строку с солью: {qrString}</h2>
-      <h3> Соль: {salt}</h3>
-      <QRCode value={salt + qrString} />
-    </div>
+    <>
+      <Layout style={{ padding: '0 24px 24px' }}>
+        <Header>
+          <Typography.Title level={3}>h3. Ant Design</Typography.Title>
+        </Header>
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+
+          <Input value={qrString} onChange={handleChange} size="large" placeholder="Введите строку для кодирования" prefix={<QrcodeOutlined />} />
+
+          <h2>Кодируем строку с солью: {qrString}</h2>
+          <h3> Соль: {salt}</h3>
+          <QRCode value={salt + qrString} />
+
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </>
   );
 }
 

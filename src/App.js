c2ff164f2a1fa067e4bb795de01053ad7dd66101
sbmart <sbmart73@gmail.com>
2021-06-21 22:16:52 +0300

    Add antd (#1)
    * intermediate push wo QF of a header
    
    * Change Layout.Header to PageHeader
    
    * tests and coverage add
    
    Co-authored-by: sbmart <sbmart@ya.ru>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,61 @@
-import './App.css';
+// import './App.css';
+import 'antd/dist/antd.css';
 import React, { useState } from 'react'
 import QRCode from 'qrcode.react'
+import { Button, PageHeader, Input, Layout, Descriptions } from 'antd'
+import { QrcodeOutlined } from '@ant-design/icons'
 
-const salt = 'методы цифровой трансформации в действии!'
+const { Footer, Content } = Layout;
+const salt = '-*методы цифровой трансформации в действии!/-'
+
 function App() {
   const [qrString, setQrString] = useState('')
 
-  function handleChange(event) {
-    setQrString(event.target.value)
+  function handleQRChange(e) {
+    setQrString(e.target.value)
   }
   return (
-    <div className="App">
-      <input value={qrString} onChange={handleChange} />
-      <h2>Кодируем строку с солью: {qrString}</h2>
-      <h3> Соль: {salt}</h3>
-      <QRCode value={salt + qrString} />
-    </div>
+    <>
+      <PageHeader
+        ghost={false}
+        title="QR виджет"
+        subTitle="сервис по переводу текста в QR-код на лету"
+        extra={[
+          <Button key="2">Тема</Button>,
+          <Button key="1" type="primary">Печатать</Button>,
+        ]}
+      >
+        <Descriptions size="small" column={2}>
+          <Descriptions.Item label="Created from stratch by">Sbmart</Descriptions.Item>
+          <Descriptions.Item label="Creation Time">2021-May-08</Descriptions.Item>
+          <Descriptions.Item label="Кодируемая строка">
+            <>{qrString}</>
+          </Descriptions.Item>
+        </Descriptions>
+      </PageHeader>
+
+      <Layout style={{ padding: '0 24px 24px' }}>
+
+        <Content
+          className="site-layout-background"
+          style={{
+            padding: 24,
+            margin: 0,
+            minHeight: 280,
+          }}
+        >
+
+          <Input value={qrString} onChange={handleQRChange} size="large" placeholder="Введите строку для кодирования" prefix={<QrcodeOutlined />} />
+          <br></br>
+          <h2>Кодируем строку с солью: {qrString}</h2>
+          <h3>Захардкоденная соль: {salt}</h3>
+          <br></br>
+          <QRCode size={256} value={salt + qrString} />
+
+        </Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </>
   );
 }
 
